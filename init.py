09a535e70d33744e1af56ab2db30fd179a398d2f
Spymac <Spymac@users.noreply.github.com>
2015-01-19 16:17:51 +0100

    Update init.py

--- init.py
+++ init.py
@@ -6,6 +6,7 @@ b = SMBus(1)
 addr = 0x62
 
 
-ret = b.read_i2c_block_data(addr, 0x81)
+#ret = b.read_i2c_block_data(addr, 0x81)
+#print (ret)
 
-print [map(hex, l) for l in ret]
+b.write_i2c_block_data(addr,0x88,[0xFF, 0xFF, 0x33, 0x00, 0x00, 0x00, 0x0F])
