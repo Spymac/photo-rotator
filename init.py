9e405531d15d9860c150fbf2b24a42995dd3216f
Reytiros <gtgan@gmx.de>
2015-01-26 22:52:40 +0100

    added main and moved functions

--- init.py
+++ init.py
@@ -41,89 +41,106 @@ b = SMBus(I2CDEV)
 def binary(num, pre='0b', length=8, spacer=0):
     return '{0}{{:{1}>{2}}}'.format(pre, spacer, length).format(bin(num)[2:])
  
-def isNegative (v):
-	return bool(v >> 15 == 1)
+def isNegative(v):
+    return bool(v >> 15 == 1)
 
 def setBit(int_type, offset):
     return(int_type | (1 << offset))
 
-def getPositionBytes (newPosition):
-	if newPosition < 0:
-		if newPosition <= -32768:
-			newPosition = 0xffff
-		else:
-			newPosition = setBit(-newPosition, 15)
-	else:
-		if newPosition >= 32767:
-			newPosition = 32767
+def getPositionBytes(newPosition):
+    if newPosition < 0:
+        if newPosition <= -32768:
+            newPosition = 0xffff
+        else:
+            newPosition = setBit(-newPosition, 15)
+    else:
+       if newPosition >= 32767:
+           newPosition = 32767
 			
-	return [newPosition >> 8, newPosition & 0x00ff]
+    return [newPosition >> 8, newPosition & 0x00ff]
 
-def setPosition (newPosition):
-	positionByte = getPositionBytes(newPosition)
-	b.write_i2c_block_data(I2CADDR,0x8B,[0xFF, 0xFF, positionByte[0], positionByte[1]])
+def setPosition(newPosition):
+    positionByte = getPositionBytes(newPosition)
+    b.write_i2c_block_data(I2CADDR,0x8B,[0xFF, 0xFF, positionByte[0], positionByte[1]])
 
-def getPosition ():
-	ret = b.read_i2c_block_data(I2CADDR, 0xFC)
-	return (ret[1] << 8) + ret[2]
+def getPosition():
+    ret = b.read_i2c_block_data(I2CADDR, 0xFC)
+    return (ret[1] << 8) + ret[2]
 
-def resetPosition ():
-	b.write_byte(I2CADDR, 0x86)
+def resetPosition():
+    b.write_byte(I2CADDR, 0x86)
 	
-def hardStop ():
-	b.write_byte(I2CADDR, 0x85)
+def hardStop():
+    b.write_byte(I2CADDR, 0x85)
 
-def gotoSecurePosition ():
-	b.write_byte(I2CADDR, 0x84)
+def gotoSecurePosition():
+    b.write_byte(I2CADDR, 0x84)
 
-def getFullStatus1 ():
-	return b.read_i2c_block_data(I2CADDR, 0x81)
+def getFullStatus1():
+    ret = b.read_i2c_block_data(I2CADDR, 0x81)
+    for i in range(1, 8):
+        print (binary(ret[i]))
+    return ret    
 
-# GETFULLSTATUS 1
-print ("GETFULLSTATUS 1:\n")
-ret = b.read_i2c_block_data(I2CADDR, 0x81)
-for i in range(1, 8):
-	print (binary(ret[i]))
- 
-# GETFULLSTATUS 2
-print ("\nACTUAL POS:\n")
- 
-ret = b.read_i2c_block_data(I2CADDR, 0xFC)
-curPos = (ret[1] << 8) + ret[2]
+def getFullStatus2():
+    ret = b.read_i2c_block_data(I2CADDR, 0xFC)
+    return ret
+    
+def setMotorParam():
+    if WRITE_RAM_ON_STARTUP:
+        b.write_i2c_block_data(I2CADDR,0x89,[0xFF, 0xFF, (lRun << 4) + lHold, (vMax << 4) + vMin, (((((secPos >> 8) & 0b0111) << 1) + shaft) << 4) + accel, secPos & 0b000011111111, (accelShape << 4) + (stepMode << 2)])
 
-if isNegative(curPos):
-	print (curPos & 0x7FFF)
-else:
-	print (-(curPos & 0x7FFF))
+def positionInit():
+    if INIT_DRIVE:
+        b.write_i2c_block_data(I2CADDR,0x88,[0xFF, 0xFF,  (vMax << 4) + vMin, targetPos1 >> 8, targetPos1 & 0x00FF, targetPos2 >> 8, targetPos2 & 0x00FF])
+  
+# not done      
+def checkError():
+    pass
+    #ret1 = getFullStatus1()
+    #ret2 = getFullStatus2()
+    
 
-
- 
-# SETMOTORPARAM
-if WRITE_RAM_ON_STARTUP:
-	b.write_i2c_block_data(I2CADDR,0x89,[0xFF, 0xFF, (lRun << 4) + lHold, (vMax << 4) + vMin, (((((secPos >> 8) & 0b0111) << 1) + shaft) << 4) + accel, secPos & 0b000011111111, (accelShape << 4) + (stepMode << 2)])
-
-# Position Init
-if INIT_DRIVE:
-	b.write_i2c_block_data(I2CADDR,0x88,[0xFF, 0xFF,  (vMax << 4) + vMin, targetPos1 >> 8, targetPos1 & 0x00FF, targetPos2 >> 8, targetPos2 & 0x00FF])
-
-setPosition(0)
-sleep(5)
-
-ret = b.read_i2c_block_data(I2CADDR, 0xFC)
-curPos = (ret[1] << 8) + ret[2]
-
-
-for i in range (0,90):
-	curPos = curPos + 35
-	setPosition(curPos)
-	print (i)
-	while True:
-		tmcPos = getPosition()
-		print(tmcPos)
-		if abs(tmcPos-curPos) < 2:
-			# INSERT PHOTO HERE
-			system("uvccapture -v -S45 -B190 -C35 -G50 -x640 -y480 -otest/test{:02}.jpg".format(i))
-			break;
-	
-	
+#MAIN 
+if __name__ == "__main__":
+    
+    # GETFULLSTATUS 1
+    print ("GETFULLSTATUS 1:\n")
+    ret = getFullStatus1()    
+     
+    # GETFULLSTATUS 2
+    print ("\nACTUAL POS:\n")
+    ret = getFullStatus2()
+    curPos = (ret[1] << 8) + ret[2]
+    
+    if isNegative(curPos):
+        print (curPos & 0x7FFF)
+    else:
+        print (-(curPos & 0x7FFF))
+    
+    # setting motor parameter to configured values
+    setMotorParam()
+    
+    # initialising position
+    positionInit()
+    
+    resetPosition
+    setPosition(0)
+    sleep(5)
+    
+    # GETFULLSTATUS 2
+    ret = getFullStatus2()
+    curPos = (ret[1] << 8) + ret[2]
+    
+    # rotate and take pictures
+    for i in range (0,90):
+        curPos = curPos + 35
+        setPosition(curPos)
+        print (i)
+        while True:
+             tmcPos = getPosition()
+             print(tmcPos)
+             if abs(tmcPos-curPos) < 2:
+                 system("uvccapture -v -S45 -B190 -C35 -G50 -x640 -y480 -otest/test{:02}.jpg".format(i))
+                 break
 	
